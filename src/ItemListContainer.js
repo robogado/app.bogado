7ae0f0c9da6c952b3e3fe2314ba649e57d7d49b5
robogado <rocchi.bogado@gmail.com>
2021-10-25 21:37:13 -0300

    Desafio 5, contador

--- src/ItemListContainer.js
+++ src/ItemListContainer.js
@@ -1,8 +1,12 @@
+import ItemCount from "./components/itemCount/ItemCount"
+
 const ItemListContainer = ({saludo}) => {
     return (
         <div>
             <h2>{saludo}</h2>
+            <ItemCount stock="10"/>
         </div>
+
     )
 }
 
---
+++ src/components/itemCount/ItemCount.js
@@ -0,0 +1,44 @@
+import React , { useState } from "react"
+
+const ItemCount = (props) => {
+
+    //Creamos stock y unidades
+    const [stock, setStock] = useState(props.stock)
+    const [unidades , setUnidades] = useState(0)
+
+    //Creamos una funcion para aumentar las unidades y en caso de no haber stock que nos avise
+    const sumaDeContador = {
+        sumaStock:()=>{
+            if (stock===0){
+                alert ("No hay suficiente stock")
+            } else {
+                setStock(stock-1)
+                setUnidades(unidades+1)
+            }
+        }
+    }
+    //Creamos una funcion para disminuir las unidades y en caso de no haber stock que nos avise
+    const restaDeContador = {
+        restaStock:()=>{
+            if (unidades===0){
+                alert ("Sume unidades, el valor ingresado debe ser mayor a 0")
+            } else {
+                setUnidades(unidades-1)
+                setStock(stock+1)
+            }
+        }
+    }
+
+
+    //Creamos los botones + y - para aumentar y disminuir las unidades
+    return (
+        <div className="botonContador">
+            <button onClick= {sumaDeContador.sumaStock}>+</button>
+            <p>{unidades}</p>
+            <button onClick= {restaDeContador.restaStock}>-</button>
+            <p>Stock disponible: {stock} </p>
+        </div>
+    )
+}
+
+export default ItemCount
